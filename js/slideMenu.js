3748f9a5b14efa702815714e58abda4b0e584f7f
kmokwena <mokwenakw@gmail.com>
2022-11-09 08:34:44 +0200

    improved site

--- js/slideMenu.js
+++ js/slideMenu.js
@@ -1,8 +1,9 @@
 function openSlideMenu() {
   document.getElementById("menu").style.width = "250px";
-  document.getElementById("content").style.marginLeft = "250px";
+  document.getElementsByClassName("fa-bars")[0].style.opacity = "0";
 }
 function closeSlideMenu() {
   document.getElementById("menu").style.width = "0";
   document.getElementById("content").style.marginLeft = "0";
+  document.getElementsByClassName("fa-bars")[0].style.opacity = "1";
 }
